8bf8814b9dadb426d69c6f36052e99af6ad22ba8
Juan Baranovsky <xxbaraxx@hotmail.com>
2023-07-19 19:28:17 -0300

    add grid in letters alphabet

--- src/components/Alphabet.tsx
+++ src/components/Alphabet.tsx
@@ -11,19 +11,19 @@ const Alphabet: FC<AlphabetProps> = ({}) => {
     <div className='flex flex-col justify-center gap-4'>
       <h4 className='text-3xl text-center'>Search cocktail by letter</h4>
       <div className='container flex flex-wrap items-center justify-center max-w-6xl gap-1.5 px-4 pb-4 mx-auto sm:gap-1 '>
-        {alphabet.map((letter, index) => (
-          <>
-            <Link
-              key={letter}
-              href={`/cocktails/letter/${letter}`}
-              className=' px-1.5 md:border-2 rounded-md border-transparent md:text-lg text-2xl hover:border-slate-200 flex bg-transparent text-slate-100 hover:bg-transparent '
-            >
-              {letter}
-            </Link>
-
-            <span>{index !== alphabet.length - 1 && '-'}</span>
-          </>
-        ))}
+        <div className='grid grid-cols-7 lg:[grid-template-columns:repeat(26,minmax(0,1fr))] gap-1 sm:[grid-template-columns:repeat(13,minmax(0,1fr))]'>
+          {alphabet.map((letter, index) => (
+            <>
+              <Link
+                key={letter}
+                href={`/cocktails/letter/${letter}`}
+                className=' px-1.5 md:border-2  justify-center rounded-md border-transparent text-2xl hover:border-slate-200 flex bg-transparent text-slate-100 hover:bg-transparent '
+              >
+                {letter}
+              </Link>
+            </>
+          ))}
+        </div>
       </div>
     </div>
   )
