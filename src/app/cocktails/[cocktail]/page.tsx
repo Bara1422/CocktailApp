2f02313c95e20de6b0d41fad574974747c181c33
Juan Baranovsky <xxbaraxx@hotmail.com>
2023-10-04 15:32:30 -0300

    add no results found in /cocktail if search from home

--- src/app/cocktails/[cocktail]/page.tsx
+++ src/app/cocktails/[cocktail]/page.tsx
@@ -13,15 +13,25 @@ const CocktailPage = async ({ params: { cocktail } }: CocktailProps) => {
   const cocktailDataFetch = getCocktailByName(cocktail)
   const drinks = await cocktailDataFetch
 
+  console.log(drinks)
+
+  const noDrinks = () => {
+    return (
+      <span className='flex items-center justify-center w-full mx-auto text-2xl font-semibold'>
+        No results found in the DB.
+      </span>
+    )
+  }
+
   return (
     <div className='flex flex-col items-center w-2/3 p-4 py-24 mx-auto'>
       <h2 className='pb-20 text-5xl'>Cocktails found:</h2>
-      <ul className='md:gap-10 gap-5 grid grid-cols-[repeat(auto-fill,200px)] md:justify-between justify-around w-full'>
-        {drinks &&
-          drinks.map((drink: Cocktail) => {
+      {drinks && (
+        <ul className='md:gap-10 gap-5 grid grid-cols-[repeat(auto-fill,200px)] justify-around w-full'>
+          {drinks.map((drink: Cocktail) => {
             const drinkNameWithHyphens = drink.strDrink.replace(/ /g, '-')
             return (
-              <div key={drink.idDrink}>
+              <li key={drink.idDrink}>
                 <Link
                   href={`/drinks/${drink.idDrink}-${drinkNameWithHyphens}`}
                   className='flex flex-col w-[200px] text-slate-400 hover:text-slate-100'
@@ -35,10 +45,12 @@ const CocktailPage = async ({ params: { cocktail } }: CocktailProps) => {
                   />
                   <h5 className='pt-1 text-center '>{drink.strDrink}</h5>
                 </Link>
-              </div>
+              </li>
             )
           })}
-      </ul>
+        </ul>
+      )}
+      {!drinks && noDrinks()}
     </div>
   )
 }
